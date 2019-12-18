c6bce7eceb78572a4e3266fcc99fd1f7a8c13ad6
Derrick <derrickip34@gmail.com>
2019-12-18 10:31:28 +0300

    Make images on home page change automatically

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    var myIndex = 0
+    carousel()
+  
+    function carousel() {
+      var i;
+      var x = $(".imageHolder")
+      for(i = 0; i < x.length; i++) {
+        x[i].style.display = "none"
+      }
+      myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); 
+    }
+  })
+  $(document).ready(function() {
+    var myIndex = 0
+    carousel()
+  
+    function carousel() {
+      var i;
+      var x = $(".images-tv")
+      for(i = 0; i < x.length; i++) {
+        x[i].style.display = "none"
+      }
+      myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 6000); 
+    }
+  })
