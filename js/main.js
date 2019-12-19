09ea3261b740eb6f361bb4d476bdd8506d154be2
Derrick <derrickip34@gmail.com>
2019-12-19 13:34:37 +0300

    Add a movie to the list

---
+++ js/main.js
@@ -0,0 +1,55 @@
+var movieName = ["Black Panther", "Sarafina", "The Lion King(2019)", "Queen of Katwe", "Sarafina", "12 Years a Slave", "Cairotime", "Sahara", "Hyenas", "Out of Africa", "African Cats", "Congo", "Nairobi Half-life"]
+var showName = ["Shuga", "Rhythm City", "Tinsel", "The Queen"]
+
+$(document).ready(function() {
+    $("#movie-slct").empty()
+    $.each(movieName, function(val, text) {
+        $("#movie-slct").append($("<option></option>").val(val).html(text))
+    })
+    $("#tv-slct").empty()
+    $.each(showName, function(val, text) {
+        $("#tv-slct").append($("<option></option>").val(val).html(text))
+    })
+    $("#movie-review").click(function() {
+      $(".review1").slideDown()
+    })
+    $("#tv-review").click(function() {
+      $(".review2").slideDown()
+    })
+  })
+  $(document).ready(function() {
+    $("#submit").click(function() {
+        var name = $("#username").val()
+        var review = $("#usereview").val()
+        var message = $("#msg").val()
+        var movieValue = $("#movie-slct option:selected").text()
+
+    if(name === "") {
+        alert("Enter your name")
+    }
+    else if(review === "") {
+        alert("Please share your thoughts " + movieValue)
+    }
+    else{
+        alert(name + ". Thank you for sharing your thoughts on " + movieValue + ".")
+    }
+    $("#reviews").append("<li>" + name + ": " + message + "(" +movieValue + ")" + "</li>")
+    })
+    $("#submit-tv").click(function() {
+        var name = $("#user-name").val()
+        var review = $("#user-review").val()
+        var msg = $("#msg").val()
+        var tvValue = $("#tv-slct option:selected").text()
+
+    if(name === "") {
+        alert("Enter your name")
+    }
+    else if(review === "") {
+        alert("Please share your thoughts on " + tvValue)
+    }
+    else{
+        alert(name + ". Thank you for sharing your thoughts on " + tvValue + ".")
+    }
+    $("#reviews-tv").append("<li>" + name + ": " + msg + "(" + tvValue + ")" + "</li>")
+    })
+  })
